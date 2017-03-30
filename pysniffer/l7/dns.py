ae9599c523a1ffcd0da20174954349788d96cd2a
Nichlas <nichlas_vanggaard@hotmail.com>
2017-03-30 13:52:33 +0200

    DNS connection tracked

--- pysniffer/l7/dns.py
+++ pysniffer/l7/dns.py
@@ -5,16 +5,28 @@ import re
 
 
 logging.basicConfig(format='%(asctime)s %(message)s')
+
+
+class Connection:
+    STATE_QUERY_SENT = 'STATE_QUERY_SENT'
+    STATE_RESPONSE_RECEIVED = 'STATE_RESPONSE_RECEIVED'
+    def __init__(self, id, time):
+        self.id = id
+        self.time = time
+        self.state = Connection.STATE_QUERY_SENT
+
 """
 Refering to RFC 1035 https://tools.ietf.org/html/rfc1035
 """
+
 class Dns:
-    #REGEX_cli = re.compile(b'\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00')
-    REGEX = b'\x01\x00\x00\x01\x00\x00\x00\x00\x00'
     QUERY_ANCOUNT = b'\0\0'
     QUERY_NSCOUNT = b'\0\0'
     PORT = 53
-    #REGEX_src = 
+
+    def __init__(self):
+        self.conntrack = {}
+
     def register(self, app):
         self.app = app
     
@@ -23,15 +35,25 @@ class Dns:
     
     async def onConnectionEstablished(self, conn):
         conn.onClientSent += self.OnClientSent
+        conn.onServerSent += self.OnServerSent
 
     async def OnClientSent(self, conn, packet):
         conn.onClientSent -= self.OnClientSent
         payload = bytes(packet['UDP'].payload)
+
         if packet.dport == Dns.PORT and \
            bytes(payload)[7:9] == Dns.QUERY_ANCOUNT and \
            bytes(payload)[9:11] == Dns.QUERY_NSCOUNT:
-            logger.info('Found DNS query')
+            id = bytes(payload[0:2])
+            logger.info(f'Found DNS query id: {id}')
+            self.conntrack[id] = Connection(id, packet.time)
 
     async def OnServerSent(self, conn, packet):
         conn.onServerSent -= self.OnServerSent
         payload = bytes(packet['UDP'].payload)
+        id = payload[0:2]
+
+        if packet.sport == Dns.PORT and \
+           id in self.conntrack:
+           logger.info(f'Found DNS response to {id}')
+           del self.conntrack[id]
