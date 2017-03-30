e578842660c5e3cfbb875341e5902cff415a189a
Nichlas <nichlas_vanggaard@hotmail.com>
2017-03-30 16:10:37 +0200

    DNS works

--- pysniffer/l7/dns.py
+++ pysniffer/l7/dns.py
@@ -14,14 +14,15 @@ class Connection:
         self.id = id
         self.time = time
         self.state = Connection.STATE_QUERY_SENT
+        self.query = str()
 
 """
 Refering to RFC 1035 https://tools.ietf.org/html/rfc1035
 """
 
 class Dns:
-    QUERY_ANCOUNT = b'\0\0'
-    QUERY_NSCOUNT = b'\0\0'
+    QUERY_ANCOUNT = 0#b'\0\0'
+    QUERY_NSCOUNT = 0#b'\0\0'
     PORT = 53
 
     def __init__(self):
@@ -39,21 +40,36 @@ class Dns:
 
     async def OnClientSent(self, conn, packet):
         conn.onClientSent -= self.OnClientSent
-        payload = bytes(packet['UDP'].payload)
+        payload = packet['UDP'].payload
 
         if packet.dport == Dns.PORT and \
-           bytes(payload)[7:9] == Dns.QUERY_ANCOUNT and \
-           bytes(payload)[9:11] == Dns.QUERY_NSCOUNT:
-            id = bytes(payload[0:2])
-            logger.info(f'Found DNS query id: {id}')
+           payload.ancount == Dns.QUERY_ANCOUNT and \
+           payload.nscount == Dns.QUERY_NSCOUNT:
+            logger.debug(payload.ancount)
+            id = payload.id
+            logger.info(f'Found DNS query id: {hex(id)}')
             self.conntrack[id] = Connection(id, packet.time)
+            self.conntrack[id].query = payload.qd.qname.decode('utf-8')
 
     async def OnServerSent(self, conn, packet):
         conn.onServerSent -= self.OnServerSent
-        payload = bytes(packet['UDP'].payload)
-        id = payload[0:2]
-
+        payload = packet['UDP'].payload
+        id = payload.id
+            
+        
         if packet.sport == Dns.PORT and \
            id in self.conntrack:
-           logger.info(f'Found DNS response to {id}')
-           del self.conntrack[id]
+            if payload.rcode != 0:
+                logger.info(f'DNS query not found for id {self.conntrack[id].query}')
+                del self.conntrack[id]
+
+            elif payload.rcode == 0 and not payload.an == None:
+                response = list()
+                logger.debug(f'length{len(payload.an)}')
+
+                for i in range(payload.ancount):
+                    if payload.an[i].type == 1:
+                        response.append(payload.an[i].rdata)
+                        logger.debug(f'{payload.an[i].rrname} {payload.an[i].type} {payload.an[i].rdata}')
+                logger.info(f'Found DNS response {[i for i in response]} to {self.conntrack[id].query}')
+                del self.conntrack[id]
