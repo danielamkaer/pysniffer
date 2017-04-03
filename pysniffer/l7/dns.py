d579de391d023834535b709a52052cbc228e34fe
Nichlas <nichlas_vanggaard@hotmail.com>
2017-04-03 11:35:31 +0200

    still needs the rcodes  from the scapy packet

--- pysniffer/l7/dns.py
+++ pysniffer/l7/dns.py
@@ -2,31 +2,25 @@ import pysniffer.l4
 import logging
 logger = logging.getLogger(__name__)
 import re
+from scapy.layers.dns import dnsqtypes
+inv_dnsqtypes = dict(zip(dnsqtypes.values(), dnsqtypes.keys()))
 
-
-logging.basicConfig(format='%(asctime)s %(message)s')
-
-
-class Connection:
-    STATE_QUERY_SENT = 'STATE_QUERY_SENT'
-    STATE_RESPONSE_RECEIVED = 'STATE_RESPONSE_RECEIVED'
+class Query:
     def __init__(self, id, time):
         self.id = id
         self.time = time
-        self.state = Connection.STATE_QUERY_SENT
         self.query = str()
 
 """
 Refering to RFC 1035 https://tools.ietf.org/html/rfc1035
 """
-
 class Dns:
-    QUERY_ANCOUNT = 0#b'\0\0'
-    QUERY_NSCOUNT = 0#b'\0\0'
+    QUERY_ANCOUNT = 0
+    QUERY_NSCOUNT = 0
     PORT = 53
 
     def __init__(self):
-        self.conntrack = {}
+        self.queries = {}
 
     def register(self, app):
         self.app = app
@@ -40,36 +34,38 @@ class Dns:
 
     async def OnClientSent(self, conn, packet):
         conn.onClientSent -= self.OnClientSent
-        payload = packet['UDP'].payload
+        if 'DNS' in packet:
+            payload = packet['DNS']
+        else:
+            return
 
         if packet.dport == Dns.PORT and \
-           payload.ancount == Dns.QUERY_ANCOUNT and \
-           payload.nscount == Dns.QUERY_NSCOUNT:
-            logger.debug(payload.ancount)
+           payload.ancount == dnsqtypes['ANY'] and \
+           payload.nscount == dnsqtypes['ANY']:
             id = payload.id
             logger.info(f'Found DNS query id: {hex(id)}')
-            self.conntrack[id] = Connection(id, packet.time)
-            self.conntrack[id].query = payload.qd.qname.decode('utf-8')
+            self.queries[id] = Query(id, packet.time)
+            self.queries[id].query = payload.qd.qname.decode('utf-8')
 
     async def OnServerSent(self, conn, packet):
         conn.onServerSent -= self.OnServerSent
-        payload = packet['UDP'].payload
+        if 'DNS' in packet:
+            payload = packet['DNS']
+        else:
+            return
         id = payload.id
-            
         
-        if packet.sport == Dns.PORT and \
-           id in self.conntrack:
+        if packet.sport == Dns.PORT and id in self.queries:
             if payload.rcode != 0:
-                logger.info(f'DNS query not found for id {self.conntrack[id].query}')
-                del self.conntrack[id]
+                logger.info(f'DNS query not found for id {self.queries[id].query}')
+                del self.queries[id]
 
             elif payload.rcode == 0 and not payload.an == None:
                 response = list()
-                logger.debug(f'length{len(payload.an)}')
 
                 for i in range(payload.ancount):
-                    if payload.an[i].type == 1:
+                    if payload.an[i].type == inv_dnsqtypes('A'):
                         response.append(payload.an[i].rdata)
                         logger.debug(f'{payload.an[i].rrname} {payload.an[i].type} {payload.an[i].rdata}')
-                logger.info(f'Found DNS response {[i for i in response]} to {self.conntrack[id].query}')
-                del self.conntrack[id]
+                logger.info(f'Found DNS response {[i for i in response]} to {self.queries[id].query}')
+                del self.queries[id]
